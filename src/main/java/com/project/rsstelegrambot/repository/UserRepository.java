21a6cc242149b57e6cc9c1e36cad20a5b5011a3b
Marina-Mironova <kenderrisha1@gmail.com>
2023-11-14 14:38:46 +0100

    UserRepository class was replaced by interface with extentions

--- src/main/java/com/project/rsstelegrambot/repository/UserRepository.java
+++ src/main/java/com/project/rsstelegrambot/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.project.rsstelegrambot.repository;
 
-public class UserRepository {
+import com.project.rsstelegrambot.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>, CrudRepository<User, Long> {
+
 }
+
+
