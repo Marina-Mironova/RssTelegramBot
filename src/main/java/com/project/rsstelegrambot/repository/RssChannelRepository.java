ed437394d86cd99d0311af60c526b3a3a327f565
Marina-Mironova <kenderrisha1@gmail.com>
2023-11-14 14:44:46 +0100

    RssChannelRepository class was replaced by interface with extentions

--- src/main/java/com/project/rsstelegrambot/repository/RssChannelRepository.java
+++ src/main/java/com/project/rsstelegrambot/repository/RssChannelRepository.java
@@ -1,4 +1,9 @@
 package com.project.rsstelegrambot.repository;
 
-public class RssChannelRepository {
+import com.project.rsstelegrambot.domain.entity.RssChannel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RssChannelRepository extends JpaRepository<RssChannel, Long>, CrudRepository<RssChannel, Long> {
+
 }
